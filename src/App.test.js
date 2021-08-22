e712839adc9642c0a4073eefcf19aacee35fb180
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-22 18:05:15 +0530

    changes in the react app

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
 	render(<App />);
-	const linkElement = screen.getByText(/Learning github actions/i);
+	const linkElement = screen.getByText(/Learning Github Actions/i);
 	expect(linkElement).toBeInTheDocument();
 });
