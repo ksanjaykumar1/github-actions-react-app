c0585dee4bb72f3295ec33e62ca19cef6f0c3138
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-26 16:38:03 +0530

    custom removed the error message

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -9,7 +9,7 @@ try{
     core.setOutput('time', time.toTimeString())
 
     console.log(JSON.stringify(github, null, '\t'))
-throw( new Error('Some error message'))
+    //throw( new Error('Some error message'))
 }
 catch(error){
     core.setFailed(error.message)
