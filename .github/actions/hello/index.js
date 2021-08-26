b64cf250b53e2c571a0efbe2b7dcf5f119183a43
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-26 16:17:47 +0530

    using private actions to greet

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,16 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try{
+    const name = core.getInput("who-to-greet");
+    console.log(`Hello ${name} `);
+
+    const time = new Date()
+    core.setOutput('time', time.toTimeString())
+
+    console.log(JSON.stringify(github, nul, '\t'))
+    //throw( new Error('Some error message'))
+}
+catch(error){
+    core.setFailed(error.message)
+}
