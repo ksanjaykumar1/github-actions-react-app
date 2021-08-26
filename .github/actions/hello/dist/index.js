06d22712271a79b7637881a20f5b5f7c0e32f87f
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-26 16:36:32 +0530

    re-builded the index.js

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -6369,8 +6369,8 @@ try{
     const time = new Date()
     core.setOutput('time', time.toTimeString())
 
-    console.log(JSON.stringify(github, nul, '\t'))
-    //throw( new Error('Some error message'))
+    console.log(JSON.stringify(github, null, '\t'))
+throw( new Error('Some error message'))
 }
 catch(error){
     core.setFailed(error.message)
